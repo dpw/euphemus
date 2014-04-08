df47fc6ea912bc338e82cd79af27797fccd34874
David Wragg <david@wragg.org>
2014-04-08 08:38:51 +0100

    Oh, the generation test template can simply be a function

--- test/test.c
+++ test/test.c
@@ -207,17 +207,65 @@ static void test_size(void)
 	check_size("{\"baz\":{},\"x\":\"y\"}", 2);
 }
 
+static void test_gen(struct eu_value value, const char *expected_cstr)
+{
+	struct eu_string_ref expected = eu_cstr(expected_cstr);
+	struct eu_generate *eg;
+	char *buf = malloc(expected.len + 1);
+	char *buf2 = malloc(expected.len + 1);
+	size_t i, len, len2;
+
+	/* Test generation in one go. */
+	eg = eu_generate_create(value);
+	len = eu_generate(eg, buf, expected.len + 1);
+	assert(len <= expected.len);
+	assert(!eu_generate(eg, buf + len, 1));
+	assert(eu_generate_ok(eg));
+	eu_generate_destroy(eg);
+	assert(eu_string_ref_equal(eu_string_ref(buf, len), expected));
+
+	/* Test generation broken into two chunks */
+	for (i = 0;; i++) {
+		eg = eu_generate_create(value);
+		len = eu_generate(eg, buf, i);
+		assert(len <= i);
+		len2 = eu_generate(eg, buf2, expected.len + 1);
+		assert(len + len2 <= expected.len);
+		assert(eu_generate_ok(eg));
+		eu_generate_destroy(eg);
+
+		memcpy(buf + i, buf2, len2);
+		assert(eu_string_ref_equal(eu_string_ref(buf, len + len2),
+					   expected));
+
+		if (len2 == 0)
+			break;
+	}
+
+	/* Test byte-at-a-time generation */
+	eg = eu_generate_create(value);
+	len = 0;
+
+	while (eu_generate(eg, buf2, 1)) {
+		assert(len < expected.len);
+		buf[len++] = *buf2;
+	}
+
+	assert(eu_generate_ok(eg));
+	eu_generate_destroy(eg);
+	assert(eu_string_ref_equal(eu_string_ref(buf, len), expected));
+
+	free(buf);
+	free(buf2);
+}
+
 static void test_gen_string(void)
 {
 	struct eu_string str;
 
 	eu_string_init(&str);
 	assert(eu_string_assign(&str, eu_cstr("hello")));
-
-#define VALUE eu_string_value(&str)
-#define EXPECTED "\"hello\""
-#include "test_gen.c"
-
+	test_gen(eu_string_value(&str), "\"hello\"");
 	eu_string_fini(&str);
 }
 
